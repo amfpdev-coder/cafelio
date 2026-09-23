e1ff5bc4ce4838d3cfeef5d7318b6b9aacb43ee5
mands <mands.cristiny@gmail.com>
2026-09-23 14:40:04 -0300

    chore: remove log de depuração do filtro JWT

--- backend/src/main/java/com/cafelio/api/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/cafelio/api/security/JwtAuthenticationFilter.java
@@ -36,13 +36,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String header = request.getHeader(HEADER);
 
-        System.out.println(
-                request.getMethod() + " " +
-                        request.getRequestURI() +
-                        " - Authorization presente: " +
-                        (header != null)
-        );
-
         if (header != null && header.startsWith(PREFIX)) {
             String token = header.substring(PREFIX.length());
 
