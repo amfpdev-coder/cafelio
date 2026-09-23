283cac4bda0f98e7d45935161e5616695d813cea
amfpdev-coder <amfp.dev@gmail.com>
2026-09-23 12:36:12 -0300

    refactor: remove import nao utilizado do AuthController

--- backend/src/main/java/com/cafelio/api/controller/AuthController.java
+++ backend/src/main/java/com/cafelio/api/controller/AuthController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.UUID;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
